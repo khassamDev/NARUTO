2d3c2551351b599bbd352db348c5a8a672803605
khassamDev <khassammc@gmail.com>
2025-08-15 14:16:21 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.clear()
-console.log('🗣️ Iniciando Michi Wa Bot...')
+console.log('🗣️ Iniciando Naruto Bot...')
 
 import { join, dirname } from 'path'
 import { createRequire } from 'module'
@@ -12,7 +12,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
 
 
-cfonts.say('✧ Michi Wa ✧', {
+cfonts.say('Naruto ', {
   font: 'block',        
   align: 'center',
   gradient: ['cyan', 'magenta'],
