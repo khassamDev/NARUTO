8cd6960db8f4f56637c0257c097665c990976ed1
khassamDev <khassammc@gmail.com>
2025-08-15 14:26:19 -0300

    Actualizar info-servers.js

--- plugins/info-servers.js
+++ plugins/info-servers.js
@@ -11,36 +11,7 @@ let handler = async (m, { conn, command }) => {
     }
   }
 
-  let info = `
-「🌿」✧─── ･ ｡ﾟ★: *.✦ .* :★. ───✧
-\`𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 SYA SURVIVALS\`
-
-> ✦↷ \`ᴊᴜɢᴀʀ ʏ ᴄᴏɴsᴛʀᴜɪʀ ᴀʟ ᴍᴀxɪᴍᴏ\`
-
-✧─── ･ ｡ﾟ★: *.✦ .* :★. ───✧
-
-\`Servidor 1 (Para Android)\`
-> Plataforma: MultiCraft Build & Mine
-> Descargar:
-> play.google.com/store/apps/details?id=com.multicraft.game
-\`Modo: Survival\`
-> Código: *Z8TY2ANB*
-\`Estado: 🟢 24/7 ON\`
-
-
-
-\`Servidor 2 (Para PC)\`
-> Versión: *1.8.8*
-> Plataforma: Minecraft Java
-> IP: *207.180.254.11:12005*
-\`Modo: Survival\`
-\`Estado: 🟢 Siempre ONLINE\`
-
-✧─── ･ ｡ﾟ★: *.✦ .* :★. ───✧
-> Comunidad sin tóxicos y con buena onda
-> Únete al *SYA TEAM* 🐾
-「🌿」✧─── ･ ｡ﾟ★: *.✦ .* :★. ───✧
-`.trim()
+  let info = `()
 
   await conn.sendMessage(m.chat, { text: info }, rcanal)
 }
